48444c28a82333c179b9d435f99531599a226d24
susan-TG <susana.tralma.g@gmail.com>
2016-11-07 13:54:29 -0300

    primer-commit

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", init);
+ 
+var todos = [];
+ 
+function agregar-tarea ()
+{
+	
+}
+ //Creamos input y boton
+    var textcont =document.createElement('input');
+    var boton =document.createElement('button');
