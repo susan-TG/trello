a043b366318c8e9dbc500c9ac5f09ceb9eede38a
susan-TG <susana.tralma.g@gmail.com>
2016-11-08 10:53:45 -0300

    modifique el navbar

--- js/javascript.js
+++ js/javascript.js
@@ -2,10 +2,36 @@ window.addEventListener("load", init);
  
 var todos = [];
  
-function agregar-tarea ()
+window.addEventListener("load", init);
+ 
+var todos = [];
+
+function form(li)
 {
-	
+	//creamos un formulario
+	var todo = document.createElement("a");
+	//href
+    todo.href = "#";
+    //añadimos varias clases
+    todo.classList.add("pull-right");
+    //añadimos el evento click para que sea eliminado
+    todo.onclick = function() 
+    { 
+        var toDelete = document.querySelector('#todo'+li.index);
+		toDelete.parentNode.removeChild(toDelete);
+    };
+    //devolvemos el nuevo elemento
+    
+    var i = document.createElement("i");
+	i.className = 'fa fa-trash fa-2x'; 
+
+	todo.appendChild(i);
+
+    return todo;
 }
- //Creamos input y boton
-    var textcont =document.createElement('input');
-    var boton =document.createElement('button');
+
+ <input type="text" id="texto" />
+var valor = document.getElementById("texto").value;
+ 
+<textarea id="parrafo"></textarea>
+var valor = document.getElementById("parrafo").value;
