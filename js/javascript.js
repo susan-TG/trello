d50fff8327ad224b3d452d9ced5d51883c9c82de
susan-TG <susana.tralma.g@gmail.com>
2016-11-09 00:47:37 -0300

    el texto de la lista se agrega en el la tarjeta

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,11 @@
 // Pasos
 // Agregar clase hidden a mi botón para que desaparezca
-// saco hidden a mi panel para que aparezca
+// Saco hidden a mi panel para que aparezca
 
 var boton = document.getElementById("agregar-tarea");
 var tarjeta = document.getElementById("tarjeta-tarea");
 
+// detecta un click en la variable boton y ejecuta la funcion transformer
 boton.addEventListener("click", transformer);
 
 function transformer() {
@@ -12,3 +13,26 @@ function transformer() {
     tarjeta.className = "panel panel-default";
 }
 
+// agregar las listas de tareas a la tarjeta
+
+// Pasos
+// Obtener el valor del input
+// Crear un elemento button dentro dl div .list-group con la clase list-group-item
+
+var btnLista = document.getElementById("lista-nueva");
+
+//detecta un click en la variable btnLista y ejecuta la funcion listaTarea
+btnLista.addEventListener("click", listaTarea);
+
+function listaTarea() {
+    var valor = document.getElementById("disabledTextInput").value;
+    var btn = document.createElement("button");
+    var texto = document.createTextNode(valor);
+
+    btn.type = "button";
+    btn.className = "list-group-item";
+    btn.appendChild(texto);
+
+    document.getElementById("lista").appendChild(btn);
+
+}
